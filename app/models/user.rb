f56a8c45d65315644c74c43cc2c34c57bcdd0e6e
Ara <hello.ara@pm.me>
2020-12-30 12:02:51 +0100

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ require 'dm-postgres-adapter'
 class User
   include BCrypt
   include DataMapper::Resource
-  # attr_reader :id, :username, :name, :email, :password_hash
+
   attr_reader :password
   attr_accessor :password_confirmation
 
@@ -19,46 +19,9 @@ class User
   property :email, String, required: true, :format => :email_address, unique: true
   property :password_digest, Text
 
-  # def initialize(id:, username:, name:, email:, password:)
-  #   @id = id
-  #   @username = username
-  #   @name = name
-  #   @email = email
-  #   @password_hash = password
-  # end
-
-  # def self.all
-  #   result = DBConnection.query("SELECT * FROM users")
-  #   result.map do |user|
-  #     User.new(id:user['id'], username:user['username'], name:user['name'], email:user['email'], password:user['password'])
-  #   end
-  # end
-
-  # def self.create(username:username, name:name, email:email, password:password)
-  #   if find(email).nil? && !username_taken?(username)
-  #     password_hash = Password.create(password)
-  #     result = DBConnection.query("INSERT INTO users(username, name, email, password) VALUES ('#{username}', '#{name}', '#{email}', '#{password_hash}') RETURNING id, username, name, email, password")
-  #     User.new(id: result[0]['id'], username: result[0]['username'], name:result[0]['name'], email:result[0]['email'], password:result[0]['password'])
-  #   end
-  # end
-
-  # def self.find(email)
-  #   result = DBConnection.query("SELECT * FROM users where email='#{email}'")
-  #   return nil if result.count.zero?
-  #   User.new(id:result[0]['id'], username:result[0]['username'], name:result[0]['name'], email:result[0]['email'], password:result[0]['password'])
-  # end
-
-  # def self.username_taken?(username)
-  #   result = DBConnection.query("SELECT * FROM users where username='#{username}'")
-  #   return false if result.count.zero?
-  #   true
-  # end
-
 
   def self.authenticate(email, password)
     user = first(email: email)
-    # return nil unless user
-    # return nil unless user.password == password
     user && Password.new(user.password_digest) == password ? user : nil
   end
 
@@ -70,6 +33,3 @@ class User
   validates_confirmation_of :password
 
 end
-
-# DataMapper.finalize
-# User.auto_upgrade!
