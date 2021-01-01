bcaf308287d65ae2812d36608c73244d96bccfba
Ara <hello.ara@pm.me>
2021-01-01 17:55:21 +0100

    add css

--- lib/seeds.rb
+++ lib/seeds.rb
@@ -4,5 +4,6 @@ require './app/models/user'
 User.create(username:'SingingBird', password:'password123', name:'Birdie', email:'birdie@example.com')
 User.create(username:'HappyCow', password:'password123', name:'Cow', email:'cow@example.com')
 User.create(username:'Chihuahua', password:'password123', name:'Doggie', email:'doggie@example.com')
-User.create(username:'Hello.Ara', password:'password123', name:'Ara', email:'hagrid@example.com')
-User.create(username:'Bob', password:'password123', name:'Bob', email:'draco@example.com')
+User.create(username:'Hello.Ara', password:'password123', name:'Ara', email:'ara@example.com')
+User.create(username:'Bob', password:'password123', name:'Bob', email:'bob@example.com')
+
